7e93e206724e8f94fcc6a60c6d80f59d1ea7b4ae
Lionheartapp <lionheartops.app@gmail.com>
2026-02-25 19:15:15 -0800

    Add basic Facilities page and Tenant layout components to fix build errors

---
+++ src/app/[tenant]/facilities/page.tsx
@@ -0,0 +1,8 @@
+export default function FacilitiesPage() {
+	return (
+		<main>
+			<h1>Facilities</h1>
+			<p>This is the facilities page.</p>
+		</main>
+	);
+}
---
+++ src/app/[tenant]/layout.tsx
@@ -0,0 +1,8 @@
+export default function TenantLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<div>
+			{/* Layout for tenant pages */}
+			{children}
+		</div>
+	);
+}
